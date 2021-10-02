760261fa8230a78781fe00f3b2506114150b4646
choijaeho <dhqkgbs@naver.com>
2021-10-02 15:26:22 +0900

    ADD PUT API

--- put/src/main/java/com/example/put/controller/PutController.java
+++ put/src/main/java/com/example/put/controller/PutController.java
@@ -1,17 +1,15 @@
 package com.example.put.controller;
 
 import com.example.put.dto.PutRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class PutController {
 
-    @PutMapping("/put")
-    public PutRequestDto put(@RequestBody PutRequestDto putRequestDto){
+    @PutMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto putRequestDto, @PathVariable Long userId){
+        System.out.println(userId);
         System.out.println(putRequestDto.toString());
         return putRequestDto;
     }

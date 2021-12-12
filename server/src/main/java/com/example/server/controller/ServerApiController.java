deab447ea59bd8228d7dca0fb86f7b2e0ee49485
choijaeho <dhqkgbs@naver.com>
2021-12-12 20:45:40 +0900

    [UPDATE] Naver 검색 API 사용해보기

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,12 +4,58 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    //Naver API 사용
+    //https://developers.naver.com/docs/serviceapi/search/local/local.md#%EC%A7%80%EC%97%AD
+    //https://openapi.naver.com/v1/search/local.xml?query=\xea\xb0\x88\xeb\xb9\x84\xec\xa7\x91&display=10&start=1&sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        //String query ="갈비집";
+        //String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));     //utf-8로 인코딩
+        // utf-8 URiComponetsBuilder에서 encode하면된다.
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query","투썸플레이스")
+                .queryParam("display",10)
+                .queryParam("start",1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        log.info("uri : {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //Header를 사용
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "?")
+                .header("X-Naver-Client-Secret" , "?")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req,String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age){

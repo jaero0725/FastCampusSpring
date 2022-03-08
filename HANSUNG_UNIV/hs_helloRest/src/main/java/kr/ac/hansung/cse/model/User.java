4784df3850be98df6d88424b3c4e098fa89a84b4
choijaeho <dhqkgbs@naver.com>
2022-03-08 23:17:16 +0900

    rest

---
+++ HANSUNG_UNIV/hs_helloRest/src/main/java/kr/ac/hansung/cse/model/User.java
@@ -0,0 +1,15 @@
+package kr.ac.hansung.cse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private long id;
+	private String name;
+	private int age;
+	private double salary;
+}

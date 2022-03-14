0d7bb1341a80e0785d8925820c11f441c5ab0092
choijaeho <dhqkgbs@naver.com>
2022-03-14 14:39:35 +0900

    get

--- HANSUNG_UNIV/hs_ecommerce/src/main/java/kr/ac/ecommerce/controller/CategroyController.java
+++ HANSUNG_UNIV/hs_ecommerce/src/main/java/kr/ac/ecommerce/controller/CategroyController.java
@@ -1,13 +1,16 @@
 package kr.ac.ecommerce.controller;
 
-
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.ac.ecommerce.dto.CategoryDto;
@@ -16,26 +19,38 @@ import kr.ac.ecommerce.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping(path="/api/categories")
+@RequestMapping(path = "/api/categories")
 @RequiredArgsConstructor
 public class CategroyController {
 
 	private CategoryService categoryService;
-	
-	//Create -POST
-	public ResponseEntity<?> createCategory(@RequestBody @Valid CategoryDto request){
-		
+
+	// Create -POST
+	@PostMapping
+	public ResponseEntity<?> createCategory(@RequestBody @Valid CategoryDto request) {
+
 		Category category = categoryService.createCategory(request.getName());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(category);
-        
+		return ResponseEntity.status(HttpStatus.CREATED).body(category);
 	}
-	//Read   -GET 
-	
-	//Read   -GET
-	
-	//Update -PUT
-	
-	//DELTE  -DELETE
-	
+
+	// Read -GET
+	@GetMapping
+    public ResponseEntity<?> retrieveAllCategories() {
+        final List<Category> categories = categoryService.getAllCategories();
+
+        if (categories.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+        return ResponseEntity.ok(categories);
+		//return new ResponseEntity<List<Category>>(categories, HttpStatus.OK);
+		
+    }
+	// Read -GET
+
+	// Update -PUT
+
+	// DELTE -DELETE
+
 }

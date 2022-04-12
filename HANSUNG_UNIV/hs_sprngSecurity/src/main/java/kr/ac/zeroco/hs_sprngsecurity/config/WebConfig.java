0a35b3a26062e2cc20e9c3c7cc19ea417cff5672
choijaeho <dhqkgbs@naver.com>
2022-04-12 22:50:28 +0900

    스프링 시큐리티 기본설정

---
+++ HANSUNG_UNIV/hs_sprngSecurity/src/main/java/kr/ac/zeroco/hs_sprngsecurity/config/WebConfig.java
@@ -0,0 +1,23 @@
+package kr.ac.zeroco.hs_sprngsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry){
+		registry.addViewController("/").setViewName("home");
+	}
+
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
+	}
+
+}
